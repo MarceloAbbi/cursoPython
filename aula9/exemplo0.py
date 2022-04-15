9494d6a6d1ad357fd998893d6411ed8b0514cc15
Marcelo Abbi <marcelo.abbi@outlook.com>
2022-04-14 23:47:48 -0300

    aula 9

---
+++ aula9/exemplo0.py
@@ -0,0 +1,8 @@
+nome = input("Qual o seu nome? ")
+idade = input("Qual a sua idade? ")
+
+ano_nascimento = 2022 - int(idade)
+
+print(f'O usuário digitou {nome} e o tipo da variável é 'f'{type(nome)}')
+print(f'{nome} tem {idade} anos.')
+print(f'{nome} nasceu no ano de {ano_nascimento}')
