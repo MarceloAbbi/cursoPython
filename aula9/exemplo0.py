bc33d6b07368cc5bb6f2c46c9529eb19f583fdf4
Marcelo Abbi <marcelo.abbi@outlook.com>
2022-04-15 18:47:47 -0300

    aulas

--- aula9/exemplo0.py
+++ aula9/exemplo0.py
@@ -6,3 +6,4 @@ ano_nascimento = 2022 - int(idade)
 print(f'O usuário digitou {nome} e o tipo da variável é 'f'{type(nome)}')
 print(f'{nome} tem {idade} anos.')
 print(f'{nome} nasceu no ano de {ano_nascimento}')
+
