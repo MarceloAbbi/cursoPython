99357385d64361576f4ab1d3fe00b2529006bc5e
Marcelo Abbi <marcelo.abbi@outlook.com>
2022-04-15 22:20:56 -0300

    aulas

--- Ifelifelse/exemplo4.py
+++ Ifelifelse/exemplo4.py
@@ -8,3 +8,9 @@ if usuario_bd == usuario and senha_bd == senha:
     print('Você está logado no sistema.')
 else:
     print('Usuário ou senha inválidos.')
+
+
+string1 = input('Digite alguma coisa: ')
+string2 = input('Digite outra coisa: ')
+
+print(f'A quantidade total de caracteres digitados foi: {len(string1) + len(string2)}')
