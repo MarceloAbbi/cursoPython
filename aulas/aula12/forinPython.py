d50d9b3a40b65f8a3c8d832fb6dda4c9d17329ec
Marcelo Abbi <marcelo.abbi@outlook.com>
2022-04-20 23:43:10 -0300

    aul

---
+++ aulas/aula12/forinPython.py
@@ -0,0 +1,16 @@
+'''
+For in em Python
+Iterando strings com for
+Função range (start=0, sop, step=1)
+'''
+
+texto = 'Python'
+
+# c = 0
+# while c < len(texto):
+#     print(texto[c])
+#     c += 1
+
+for n, letra in enumerate(texto):
+    print(n, letra)
+
