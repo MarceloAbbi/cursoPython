dc0949879e2601aa910e57b4159e2f4c15771ac2
Marcelo Abbi <marcelo.abbi@outlook.com>
2022-04-24 00:17:38 -0300

    aulas

--- aulas/aula12/forelse.py
+++ aulas/aula12/forelse.py
@@ -4,8 +4,23 @@ For / Else em Python
 
 variavel = ['Marcelo Abbi', 'Joãozinho', 'Maria']
 
+comeca_com_j = False
 for valor in variavel:
     if valor.startswith('J'):
-        print(valor, 'Começa com J')
-    else:
-        print(valor, 'Não começa com J')
+        comeca_com_j = True
+
+if comeca_com_j:
+    print('Existe uma palavra que começa com J.')
+else:
+    print('Não existe uma palavra que começa com J.')
+
+comeca_com_m = False
+for valor in variavel:
+    if valor.lower().startswith('m'):
+        comeca_com_j = True
+
+if comeca_com_m:
+    print('Existe uma palavra que começa com m.')
+else:
+    print('Não existe uma palavra que começa com m.')
+
