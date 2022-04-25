bf13fa96b1a98b777609caa0a3a40a9c4cd33d44
Marcelo Abbi <marcelo.abbi@outlook.com>
2022-04-25 20:01:31 -0300

    aulas

--- aulas/aula12/lista.py
+++ aulas/aula12/lista.py
@@ -1,4 +1,25 @@
 string = 'O Brasil é penta.'
 lista = string.split(' ')
+# string2 = ' '.join(lista)
+#
+# print(string)
+# print(lista)
+# print(string2)
 
-print(lista)
+for indice, valor in enumerate(lista):
+    print(indice, valor, lista[indice])
+
+lista2 = [
+    [0, 'Marcelo'],
+    [1,'João'],
+    [2,'Maria'],
+]
+
+for indice, nome in lista2:
+    print(indice, nome)
+
+lista3 = ['Marcelo', 'João', 'Maria']
+
+n1, n2, n3 = lista3
+
+print(n2)
